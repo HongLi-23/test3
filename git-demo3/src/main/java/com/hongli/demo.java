ad7b842f64b5d04653ecb0795c316f15709f5ebb
HongLi <hongli921123@163.com>
2019-11-12 14:11:27 +0800

    tijiao1

---
+++ git-demo3/src/main/java/com/hongli/demo.java
@@ -0,0 +1,15 @@
+package com.hongli;
+
+/**
+ * @ClassName demo
+ * @Description
+ * @Author HL
+ * @Date 2019/11/12  14:11
+ * @Version 1.0
+ */
+public class demo {
+    public static void main(String[] args) {
+
+        System.out.println(123);
+    }
+}
